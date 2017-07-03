1d130827bf78d8691ebaf25b8775a662295f39db
William Swihart <wdswihart@gmail.com>
2017-07-02 21:28:34 -0500

    Changed gui.py to Gui,py
    Separated main function and GUI functions into separate files; GUI implementation is object-oriented, in Gui.py, and the main function is contained in main.py.

--- src/python/gui.py
+++ src/python/gui.py
@@ -1,27 +1,35 @@
-# gui is the GUI implementation for the project in Linear Algebra,
+# Gui represents a GUI object, for a Linear Algebra Project on fractal compression,
 # using Python 2.7.
 #
-# William Swihart
+# William Swihart, Nick Phillips
 # University of Central Arkansas
 # Summer 2017
 
 import wx # wxPython 3.0 amd-64 (made for Python 2.7)
 
-# FUNCTIONS
-
-# initComponents initializes the frame.
-#	IN: void
-#	OUT: the app and the frame
-def initComponents():
-	app = wx.App()
-	frame = wx.Frame(None, -1, 'Fractal Compression')
-	frame.Show()
-	return app, frame
-
-# MAIN
-
-# Create the app and the frame.
-app, frame = initComponents()
-
-# Start the main loop.
-app.MainLoop()
+class Gui(wx.Frame):
+	# INITIALIZERS
+	
+	def initUI(self):
+		menuBar = wx.MenuBar()
+		
+		fileMenu = wx.Menu()
+		exitItem = fileMenu.Append(wx.ID_EXIT, 'Exit', 'Exit to Desktop')
+		menuBar.Append(fileMenu, 'File')
+		self.SetMenuBar(menuBar)
+		
+		self.Bind(wx.EVT_MENU, self.OnQuit, exitItem)
+		
+		self.SetSize(800, 400)
+		self.SetTitle('Fractal Compression')
+		self.Centre()
+		self.Show()
+	
+	def _init_(self, parent, title):
+		super(Gui, self)._init_(parent, title)
+		self.initUI()
+		
+	# METHODS
+		
+	def OnQuit(self, e):
+		self.Close()
